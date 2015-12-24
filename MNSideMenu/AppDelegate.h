6415d39ecc473a6218a2f5c0b9dcd736bd5229b9
mnn <mnn@mnndemac-mini.lan>
2015-12-24 09:24:32 +0800

    Initial Commit

---
+++ MNSideMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MNSideMenu
+//
+//  Created by mnn on 15/12/24.
+//  Copyright © 2015年 mnn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
